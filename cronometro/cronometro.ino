3d2f7a7e1af862ad719b6f12bf94b85bdd5f7aeb
Hygor Jardim <hygorjardim@gmail.com>
2017-04-20 18:38:57 -0300

    Modificação do minuto
    Contém bugs no display

--- cronometro/cronometro.ino
+++ cronometro/cronometro.ino
@@ -28,9 +28,9 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 void setup() {
   lcd.begin(16, 2);
   lcd.setCursor(0, 0);
-  lcd.print("Tangaras:  :   ");
+  lcd.print("Tangaras:00:   ");
   lcd.setCursor(0, 1);
-  lcd.print("Fonte Boa:  :   ");
+  lcd.print("Fonte Boa:00:   ");
   pinMode(Botao1, OUTPUT);
   pinMode(Botao2, OUTPUT);
   pinMode(Buzzer, OUTPUT);
@@ -66,11 +66,24 @@ void loop() {
   }
 
   delay(1000);
+  if (segundo1 == 60){
+    segundo1 = 0;
+    minuto1 = minuto1+1;
+  }   
+  if (segundo2 == 60){
+    segundo2 = 0;
+    minuto2 = minuto1+1;
+  }   
+
   segundo1 = segundo1 + 1;
   segundo2 = segundo2 + 1;
   lcd.setCursor(12, 0);
   lcd.print(segundo1);
+  lcd.setCursor(9, 0);
+  lcd.print(minuto1);
   lcd.setCursor(13, 1);
   lcd.print(segundo2);
+  lcd.setCursor(10, 1);
+  lcd.print(minuto2);
 
 }

8c0d321638abaaece550eaa7fcfdba0d5c1a4529
Hygor Jardim <hygorjardim@gmail.com>
2017-04-20 18:18:53 -0300

    Modificação do tempo

--- cronometro/cronometro.ino
+++ cronometro/cronometro.ino
@@ -1,14 +1,17 @@
 /***************************************\
 **
-Autores: Hygor Jardim e Heitor Monteiro
-Projeto Crônometro Ônibus
+  Autores: Hygor Jardim e Heitor Monteiro
+  Projeto Crônometro Ônibus
 **
-\***************************************/
+  \***************************************/
 #include <LiquidCrystal.h>
 int freq = 0;
-int tempo = 0;
-int tempo2 = 0;
-boolean iniciar = true; 
+int segundo1 = 0;
+int segundo2 = 0;
+int minuto1 = 0;
+int minuto2 = 0;
+
+boolean iniciar = true;
 
 
 int Botao1 = 8;
@@ -23,49 +26,51 @@ int Buzzer = 6;
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
-lcd.begin(16, 2);
-lcd.setCursor(0,0);
-lcd.print("Tangaras: ");
-lcd.setCursor(0,1);
-lcd.print("Fonte Boa: ");
-pinMode(Botao1, OUTPUT);
-pinMode(Botao2, OUTPUT);
-pinMode(Buzzer, OUTPUT);
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("Tangaras:  :   ");
+  lcd.setCursor(0, 1);
+  lcd.print("Fonte Boa:  :   ");
+  pinMode(Botao1, OUTPUT);
+  pinMode(Botao2, OUTPUT);
+  pinMode(Buzzer, OUTPUT);
 }
 
 void loop() {
 
-EstadoBotao1 = digitalRead(Botao1);
-EstadoBotao2 = digitalRead(Botao2);
+  EstadoBotao1 = digitalRead(Botao1);
+  EstadoBotao2 = digitalRead(Botao2);
 
-if (EstadoBotao1 == HIGH){
-tempo = 0; 
-lcd.clear();
-lcd.setCursor(0,0);
-lcd.print("Tangaras: ");
-lcd.setCursor(0,1);
-lcd.print("Fonte Boa: ");
-lcd.setCursor(11,0);
-lcd.print(tempo);
-}
+  if (EstadoBotao1 == HIGH) {
+    segundo1 = 0;
+    minuto1 = 0;
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Tangaras:  :   ");
+    lcd.setCursor(0, 1);
+    lcd.print("Fonte Boa:  :   ");
+    lcd.setCursor(12, 0);
+    lcd.print(segundo1);
+  }
 
-if (EstadoBotao2 == HIGH){
-tempo2 = 0; 
-lcd.clear();
-lcd.setCursor(0,0);
-lcd.print("Tangaras: ");
-lcd.setCursor(0,1);
-lcd.print("Fonte Boa: ");
-lcd.setCursor(11,0);
-lcd.print(tempo2);
-}
+  if (EstadoBotao2 == HIGH) {
+    segundo2 = 0;
+    minuto2 = 0;
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Tangaras:  :   ");
+    lcd.setCursor(0, 1);
+    lcd.print("Fonte Boa:  :   ");
+    lcd.setCursor(12, 0);
+    lcd.print(segundo2);
+  }
 
-delay(1000);
-tempo = tempo + 1;
-tempo2 = tempo2 + 1;
-lcd.setCursor(11,0);
-lcd.print(tempo);
-lcd.setCursor(11,1);
-lcd.print(tempo2);
+  delay(1000);
+  segundo1 = segundo1 + 1;
+  segundo2 = segundo2 + 1;
+  lcd.setCursor(12, 0);
+  lcd.print(segundo1);
+  lcd.setCursor(13, 1);
+  lcd.print(segundo2);
 
 }
